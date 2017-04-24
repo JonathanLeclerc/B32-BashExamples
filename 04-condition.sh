9b3f48c6e9b0991238bf35644dc54568bfa2f62c
Jonathan Leclerc <jonathanleclerc63@gmail.com>
2017-04-24 17:56:16 -0400

    Condition done!

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+note=60
 
+if test $note -lt 60
+then
+	echo "YOU FAILED!!!"
+elif test $note -eq 60
+then
+	echo "ouf! c'est juste!"
+else
+	echo "Alright!"
+fi
 
+read -p "Entrex une lettre: " lettre
 
+case $lettre in
+	[[:lower:]])
+		echo "Lettre en minuscule"
+		;;
+	*)
+		echo "Majuscule"
+		;;
+esac
 
+if test $lettre!="a"
+then
+	echo "la lettre n'est pas a"
+fi
 
 
 
