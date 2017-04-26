ad822180522606b163cb26d1126caff499b21295
Jonathan Leclerc <jonathanleclerc63@gmail.com>
2017-04-26 15:16:21 -0400

    Test sur les fichiers

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# $# = Nombre de parametres passes au programme
+# -ge = greater ou equal
+# -e = pour verifier si le fichier existe
+if test $# -ge 1
+then
+	if test -e $1
+	then
+		echo "Il existe"
+	fi
+fi
