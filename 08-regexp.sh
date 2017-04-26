38a1dd27d602c5a292be3c84da45b9566b5e200d
Jonathan Leclerc <jonathanleclerc63@gmail.com>
2017-04-26 15:10:55 -0400

    On peut faire des exp, regulieres

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+# Comme read -p "Lettre: " lettre
+echo -n "Lettres:"
+read lettres
+
+# ~ = regexp
+# le plus est pour plus 1
+# ^[a-z\s] le backslash est pour les espaces et puvoir faire des phrases
+if [[ $lettres =~ ^[0-9]+$  ]]
+then
+	echo "C'est un nombre"
+fi
