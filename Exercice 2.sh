c25217552f24c2c564ffbebb6e0686d967e7db3f
Jonathan Leclerc <jonathanleclerc63@gmail.com>
2017-04-26 16:02:12 -0400

    exercice 2 done!

--- Exercice 2.sh
+++ Exercice 2.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+afficherMenu(){
+	clear
+	echo "	Choix 1"
+	echo "	Choix 2"
+	echo "	appuyez sur q pour quitter"
+}
+
+choix="z"
+
+while test $choix != "q"
+do
+	afficherMenu
+	read -p "Entrez votre choix: " choix
+
+done
